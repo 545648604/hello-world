7b27a98bbd842ae07ec924e1e303d3fe9ce233f8
Jankinsu <51951944+Jankinsu@users.noreply.github.com>
2020-12-20 22:59:46 +0800

    Add files via upload
    resteps

---
+++ resteps.py
@@ -0,0 +1,11 @@
+import requests
+import time
+import random
+username = process.env.username
+password = process.env.password
+step = 20000 + round(10000*random.random())
+url = 'https://api.muxui.com/release/parameter_api?user=' + username +'&password=' + password + '&step=' + str(step)
+headers = {'User-Agent':'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.60'}
+res = requests.get(url,headers=headers,timeout=30)
+text = res.text
+print(text)
