d6f5ea4717680bb5c9c1c977173f335acae6a321
Jankinsu <51951944+Jankinsu@users.noreply.github.com>
2020-12-20 23:13:57 +0800

    Update resteps.py

--- resteps.py
+++ resteps.py
@@ -1,8 +1,8 @@
 import requests
 import time
 import random
-username = process.env.username
-password = process.env.password
+username = process.env.USER_NAME
+password = process.env.PASSWORD
 step = 20000 + round(10000*random.random())
 url = 'https://api.muxui.com/release/parameter_api?user=' + username +'&password=' + password + '&step=' + str(step)
 headers = {'User-Agent':'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.60'}
