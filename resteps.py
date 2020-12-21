1ab884bbd30bf13f346e0eef44009db14ce75639
Jankinsu <51951944+Jankinsu@users.noreply.github.com>
2020-12-21 20:02:37 +0800

    Update resteps.py

--- resteps.py
+++ resteps.py
@@ -13,6 +13,6 @@ print(texts)
 murl = "https://sc.ftqq.com/SCU131060T694c69e64cf069c6246074d14eb8aab95fc3a05dcd859.send?text="
 text = "小米运动步数修改"
 desp = "修改成功，步数修改为:" + str(step)
-uurl = murl+text+"desp="+desp
+uurl = murl+text+"&desp="+desp
 message = requests.get(murl,timeout=30)
 print(message)
